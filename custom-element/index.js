0ba45cf37a7186ea7e5bef3b85e452380515170d
sb33333 <springbean3@outlook.kr>
2024-09-16 17:45:16 +0900

    09.16.

--- custom-element/index.js
+++ custom-element/index.js
@@ -1,18 +1,40 @@
 const findValueFrom = function (scope, selector) {
     var input = scope.querySelector(selector);
-    return input.value;
+    return input?.value;
 }
 
 class CustomTable extends HTMLElement {
     
     #records = [];
     #fieldNames = null;
-    _recordExtractor = null;
+    _recordExtractor (customElement) {
+        throw new Error("not implemented.");
+    }
+    _fieldExtractor (recordElement, hasId) {
+        throw new Error("not implemented.");
+    }
     _idSymbol = Symbol("id");
     _elementSymbol = Symbol("element");
 
-    convert () {
-        return this._recordExtractor(this);
+    readFromHtml () {
+        return Array.from(this._recordExtractor(this)).map(element => {
+            var record = this._fieldExtractor(element);
+            if (element[this._idSymbol]) {
+                record[this._idSymbol] = element[this._idSymbol];
+            } else {
+                var uuid = crypto.randomUUID();
+                element[this._idSymbol] = uuid;
+                record[this._idSymbol] = uuid;
+            }
+            record[this._elementSymbol] = element;
+            return record;
+        });
+        
+        
+    }
+
+    render () {
+        throw new Error("not implemented");
     }
     
     constructor (fieldNames) {
@@ -26,30 +48,51 @@ class CustomTable extends HTMLElement {
     }
 
     connectedCallback () {
-        this.#records = this.convert();
+        this.#records = this.readFromHtml();
     }
 
 }
 
 class CustomTable2 extends CustomTable {
     constructor () {
-        super(["custNm", "age"]);
+        super(["custNm", "age", "hasCar", "status"]);
     }
 
-    _recordExtractor = function (parentElement) {
-        return Array.from(parentElement.querySelectorAll("tbody tr")).map(element => {
-            return this._fieldExtractor(element, this._idSymbol in element);
-        });
+    _recordExtractor (customElement) {
+        return Array.from(customElement.querySelectorAll("tbody tr"));
     }
-    _fieldExtractor = function (recordElement, hasId) {
+    _fieldExtractor (recordElement, hasId) {
         var obj = {
             custNm:findValueFrom(recordElement, "[name=custNm]"),
             age:findValueFrom(recordElement, "[name=age]"),
+            hasCar:findValueFrom(recordElement, "[name^=hasCar][checked]"),
+            status:findValueFrom(recordElement, "[name=status]"),
             [this._elementSymbol]: recordElement
         }
         if (!hasId) obj[this._idSymbol] = crypto.randomUUID();
 
         return obj;
+    }
+
+    render () {
+        var template = document.querySelector("#row-template").content;
+        this.records.forEach((record, index) => {
+            var clone = template.cloneNode(true);
+            for (var prop in record) {
+                var fields = Array.from(clone.querySelectorAll(`[data-name=${prop}]`));
+                fields.forEach(f => {
+                    if (f.tagName === "INPUT" && (f.type === "radio" || f.type === "checkbox")) {
+                        f.name = `${prop}_${index}`;
+                        if(f.value === record[prop]) f.checked = true;
+                    } else {
+                        f.name = prop;
+                        f.value = record[prop];
+                    }
+                })
+            }
+
+            record[this._elementSymbol].replaceWith(clone);
+        });
     }
 }
 
