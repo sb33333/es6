eb46ca2de757bde4b59e0c12478ab73f8ab86242
sb33333 <97681301+sb33333@users.noreply.github.com>
2024-09-26 15:30:58 +0900

    module-loader 업데이트

---
+++ module-loader/nested.js
@@ -0,0 +1,11 @@
+import * as module1 from "./module1.js";
+import * as module2 from "./module2.js";
+import * as module3 from "./module3.js";
+
+var test = function () {
+console.log(module1.greeting());
+console.log(module2.default);
+return module3.greeting();
+};
+
+export {t as greeting};
