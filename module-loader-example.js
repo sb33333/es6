d576d2abb05c50413b95a512f2fb3d87fc1eede1
sb33333 <springbean3@outlook.kr>
2024-10-02 21:49:55 +0900

    10.02.

--- module-loader-example.js
+++ module-loader-example.js
@@ -1,10 +1,11 @@
 ModuleLoader
-    .add("./module1.js")
-    .add("./module2.js", "ns2")    // export default requires namespace
-    .add("./module3.js")    // overwrite 
-    .add("/async/async.js", "async")
-    .init(null, true);
-    ;
+        .add("./module1.js")
+        // .add("./module2.js")    // export default requires namespace
+        .add("./module2.js", "ns2")
+        // .add("./module3.js")    // overwrite 
+        .add("/async/async.js", "async")
+        .init(null, true);
+        ;
 
 
 // ModuleLoader.add("./nested.js").init();
