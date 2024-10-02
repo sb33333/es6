65c16dfc5dbf69d9403d8f20b22ef14f4107bfb1
MUNA-desktop <springbean3@outlook.kr>
2024-10-03 07:25:11 +0900

    10.03

--- async-example.js
+++ async-example.js
@@ -2,6 +2,14 @@ document.querySelector("#printAjaxData").addEventListener("click", function () {
     var dataOut = document.querySelector("#dataOut");
     async.data.then(data => {
         dataOut.innerHTML = data.ajaxData.map(d => d.desc);
-    })
-    
+    });
+});
+
+document.querySelector("#printAjaxData2").addEventListener("click", function() {
+    var dataOut = document.querySelector("#dataOut2");
+    $.ajax({
+        url: "/data1"
+    }).done(function(res) {
+        dataOut.innerHTML = res.ajaxData.map(d => d.code);
+    });
 });
